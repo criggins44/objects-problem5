478c34a21fb0b29d8b68ddb4c53a3cdc01cef94f
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-28 13:34:57 -0400

    added comments to explain code logic

--- script.js
+++ script.js
@@ -2,10 +2,7 @@
 
 /*
 Write a JavaScript function to sort the following array of objects by title value.
-
 Hint: Read about the JavaScript sort method.
-
-Example array: 
 */
 const arrayOfBooks = [ 
 
@@ -16,12 +13,21 @@ const arrayOfBooks = [
    { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
 
    ];
+
 const arrayOfSortedObjects = arrayOfBooks.sort(function(a, b) {
+//variable that stores the sorted array of objects
+	
 	if (a.title.toLowerCase() < b.title.toLowerCase()){return -1};
+/*
+if the title property of the object smaller than the one after it, 
+it is placed earlier
+*/
+
 	if(a.title.toLowerCase() > b.title.toLowerCase()){return 1};
+//if the title property is larger than the one after it, it is placed later
+	
 	return 0;
 })
-//console.log(arrayOfSortedObjects)
 
 const sortTheObjects = arrayOfSortedObjects => {
 
